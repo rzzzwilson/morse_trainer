a886ee8f7a34c07fe5f2c2059add784eabe8e655
Ross Wilson <rzzzwilson@gmail.com>
2017-01-10 19:48:32 +0700

    Added new widget for Send speeds

---
+++ morse_trainer/send_speeds.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+A PyQt5 custom widget used by Morse Trainer.
+
+Used to select overall word speed for the Send tab only.
+
+speed = Speeds()
+
+speed.setState(active, wpm)     # sets the use checkbox and speed display
+
+The widget generates a signal '.changed' when some value changes.
+"""
+
+import platform
+from random import randint
+
+from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QLabel, QSpinBox, QGroupBox, QCheckBox
+from PyQt5.QtCore import pyqtSignal
+
+import utils
+import logger
+log = logger.Log('debug.log', logger.Log.CRITICAL)
+
+
+class Speeds(QWidget):
+
+    # signal raised when user changes cwpm
+    changed = pyqtSignal(bool, int)
+
+    # default use state
+    DefaultUseState = True
+
+    # maximum and minimum speeds
+    MinSpeed = 5
+    MaxSpeed = 40
+
+    def __init__(self, use_state=DefaultUseState, speed=MinSpeed):
+        QWidget.__init__(self)
+
+        # define state variables
+        self.use_state = use_state
+        self.speed = speed
+
+        # define the UI
+        self.initUI()
+        self.setWindowTitle('Test Send Speeds widget')
+        self.setFixedHeight(80)
+        self.show()
+
+    def initUI(self):
+        # define the widgets we are going to use
+        self.cb_use = QCheckBox('Control speed', self)
+        self.cb_use.setChecked(self.use_state)
+
+        lbl_speed = QLabel('Speed')
+        self.spb_speed = QSpinBox(self)
+        self.spb_speed.setMinimum(Speeds.MinSpeed)
+        self.spb_speed.setMaximum(Speeds.MaxSpeed)
+        self.spb_speed.setSuffix(' wpm')
+        self.spb_speed.setValue(self.speed)
+
+        # start the layout
+        layout = QVBoxLayout()
+
+        groupbox = QGroupBox("Speed")
+        groupbox.setStyleSheet(utils.StyleCSS)
+        layout.addWidget(groupbox)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.cb_use)
+        hbox.addWidget(self.spb_speed)
+        hbox.addStretch()
+
+        groupbox.setLayout(hbox)
+
+        self.setLayout(layout)
+
+        # connect spinbox events to handlers
+        self.cb_use.stateChanged.connect(self.handle_use_change)
+        self.spb_speed.valueChanged.connect(self.handle_speed_change)
+
+    def handle_use_change(self, use_state):
+        """The Use State checkbox changed.
+
+        state  boolean with new checkbox state
+
+        Raise self.changed event with params.
+        """
+
+        # save changed speed
+        self.use_state = use_state
+
+        # tell the world there was a change
+        self.changed.emit(self.use_state, self.speed)
+
+    def handle_speed_change(self, word_speed):
+        """The widget speed changed.
+
+        word_speed  the new speed
+
+        Raise self.changed event with params.
+        """
+
+        # save changed speed
+        self.speed = word_speed
+
+        # tell the world there was a change
+        self.changed.emit(self.use_state, self.speed)
+
+    def setState(self, use_state, wpm):
+        """Set the overall widget state.
+
+        use_state  the checkbox state (boolean)
+        wpm        the speed in words per minute (integer)
+        """
+
+        self.use_state = use_state
+        self.cb_use.setChecked(use_state)
+
+        self.speed = wpm
+        self.spb_words.setValue(wpm)
+
+        self.update()
+
+    def getState(self):
+        """Return the widget state.
+
+        Returns a tuple: (use_state, speed)
+        """
+
+        return (self.use_state, self.speed)
---
+++ morse_trainer/test/test_send_speeds.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Test the 'Send speeds' widget.
+"""
+
+import sys
+from send_speeds import Speeds
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout
+
+class SpeedsExample(QWidget):
+    """Application to demonstrate the Morse Trainer 'Send speeds' widget."""
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.speed_group = Speeds()
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.speed_group)
+        self.setLayout(hbox)
+
+        self.setWindowTitle('Example of Send Speeds widget')
+        self.setFixedSize(400, 125)
+        self.show()
+
+        # connect the widget to '.changed' event handler
+        self.speed_group.changed.connect(self.speed_changed)
+
+    def speed_changed(self, use_state, speed):
+        print('Changed state=%s' % str(use_state))
+        print('Changed speed=%d' % speed)
+
+
+app = QApplication(sys.argv)
+ex = SpeedsExample()
+sys.exit(app.exec())
