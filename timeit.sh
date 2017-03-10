455df42ada53cecee033e279ce02b59e32e92a42
Ross Wilson <rzzzwilson@gmail.com>
2017-03-10 11:04:20 +0700

    More convenient logging

--- timeit.sh
+++ timeit.sh
@@ -1,6 +1,7 @@
 #!/usr/local/bin/bash
 #
-# Script to time execution of 'morse_trainer'.
+# Script to time execution of 'morse_trainer'
+# under various situations.
 #
 
 LOG=timeit.log
@@ -10,12 +11,23 @@ CRASHDIR=~/Library/Logs/DiagnosticReports
 
 touch $LOG
 
+# figure out log dir to save crashes in
+if [ $# -eq 1 ]; then
+    SUBDIR=$1
+    SAVE=$SAVE/${SUBDIR}_logs
+    LOG=$SAVE/$LOG
+fi
+
+# create crash save dir if required
+if [ ! -d $SAVE ]; then
+    mkdir -p $SAVE
+fi
+
 CWPM=$(grep "copy_cwpm" $STATE | sed -e "s/^.*: //" | sed -e "s/,//")
 WPM=$(grep "copy_wpm" $STATE | sed -e "s/^.*: //" | sed -e "s/,//")
 
 # we turn crash reporting off for the test, but force it back on if interrupted
-#trap 'defaults write com.apple.CrashReporter DialogType crashreport' SIGINT
-trap 'defaults write com.apple.CrashReporter DialogType crashreport; echo "ON"' SIGINT
+trap 'defaults write com.apple.CrashReporter DialogType crashreport' SIGINT
 defaults write com.apple.CrashReporter DialogType none
 
 while true; do
@@ -31,5 +43,5 @@ while true; do
     mv $CRASHDIR/* $SAVE
 done
 
-# turn crash reporting back on
+# if we get here, turn crash reporting back on
 defaults write com.apple.CrashReporter DialogType crashreport
